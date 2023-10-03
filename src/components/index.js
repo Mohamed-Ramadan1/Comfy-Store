0069c022fa4df626de7059e647f2bf95e2f47509
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2023-10-03 15:49:53 +0300

    start featching data.

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as SubmitBtn } from "./SubmitBtn";
 export { default as Navbar } from "./Navbar";
 export { default as NavLinks } from "./NavLinks";
 export { default as Hero } from "./Hero";
+export { default as ErrorElement } from "./ErrorElement";
