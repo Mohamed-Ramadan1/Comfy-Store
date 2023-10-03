56767c95a7faceb8103aa009364a20284f243677
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2023-10-03 19:21:21 +0300

    Ending the single profuct page.

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,5 +1,5 @@
 import { useLoaderData, Link } from "react-router-dom";
-import { formatPrice, customFetch } from "../utils";
+import { formatPrice, customFetch, generateAmountOptions } from "../utils";
 import { useState } from "react";
 
 export const loader = async ({ params }) => {
@@ -80,9 +80,10 @@ const SingleProduct = () => {
               value={amount}
               onChange={handleAmount}
             >
-              <option value={1}>1</option>
+              {/* <option value={1}>1</option>
               <option value={2}>2</option>
-              <option value={3}>3</option>
+              <option value={3}>3</option> */}
+              {generateAmountOptions(20)}
             </select>
           </div>
           {/* CART BUTTON */}
