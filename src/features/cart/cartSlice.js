b2a57e25a41e7110bce8106be9cec0b8f75ebb32
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2023-10-06 10:43:42 +0300

    Setup redux-toolkit as the state mangement of the application.

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const defaultState = {
+  cartItems: [],
+  numItemsInCart: 0,
+  cartTotal: 0,
+  shipping: 500,
+  tax: 0,
+  orderTotal: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: defaultState,
+  reducers: {
+    addItem: (state, action) => {
+      console.log(action.payload);
+    },
+    clearCart: (state) => {},
+
+    removeItem: (state, action) => {},
+    editItem: (state, action) => {},
+  },
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice.reducer;
