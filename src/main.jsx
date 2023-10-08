4debd5ea654229db192b0ebfea8a0a3d04ca3752
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2023-10-08 08:42:24 +0300

    restart the application to personal resons.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-import 'react-toastify/dist/ReactToastify.css';
-import './index.css';
-import { ToastContainer } from 'react-toastify';
-import { store } from './store.js';
-import { Provider } from 'react-redux';
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
+import { ToastContainer } from "react-toastify";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-    <ToastContainer position='top-center' />
-  </Provider>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
