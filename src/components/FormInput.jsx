432b177374aa1033079864b429458c21b022ed10
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2023-10-10 14:29:12 +0300

    ening

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,19 +1,16 @@
-import React from "react";
-
-const FormInput = ({ name, type, label, defualtValue }) => {
+const FormInput = ({ label, name, type, defaultValue, size }) => {
   return (
-    <div className="form-control">
-      <label className="label" htmlFor={name}>
-        <span className="label-text capitalize">{label}</span>
+    <div className='form-control'>
+      <label htmlFor={name} className='label'>
+        <span className='label-text capitalize'>{label}</span>
       </label>
       <input
         type={type}
         name={name}
-        className="input input-bordered w-full max-w-xs"
-        defaultValue={defualtValue}
+        defaultValue={defaultValue}
+        className={`input input-bordered ${size}`}
       />
     </div>
   );
 };
-
 export default FormInput;
